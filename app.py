3531b6a57df7f3d0177e654f606772fb47ab60fc
fireph <fireph@users.noreply.github.com>
2018-07-10 18:42:09 -0700

    update to allow running the .py file directly

--- app.py
+++ app.py
@@ -82,3 +82,6 @@ def main():
             "None" if current_temps is None else current_temps[1],
             table.__html__())
     return html
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8080)
